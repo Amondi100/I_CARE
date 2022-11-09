e6668ed1787c8685939033a1285a72042cc395d4
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-11-09 15:29:55 +0300

    login

--- i_care/lib/screens/login.dart
+++ i_care/lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:i_care/utils/app_const.dart';
+import 'package:get/get.dart';
+import 'package:i_care/screens/sign_up.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -7,16 +8,99 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //APPBAR
-      appBar: AppBar(title: Text('Adventure'), actions: [
-        IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.search,
+      body: Container(
+        width: Get.size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: Get.size.height * .3,
+                width: Get.size.width * 0.3,
+                child: Column(children: [
+                  Text(
+                    "Login",
+                  ),
+                  const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )
+                ]),
+              ),
+              //'Adding Space '/
+              SizedBox(
+                height: 18,
+              ),
+              //"text "/
+              Text(
+                "Login",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5!
+                    .copyWith(fontWeight: FontWeight.bold, letterSpacing: 1.2),
+              ),
+
+              //"Adding Space"
+              SizedBox(
+                height: 30,
+              ),
+
+              //"Our Custom Text Field"/
+              //TxtField(
+              //inputType: TextInputType.emailAddress,
+              //hintText: 'Email',
+              //obscureText: false),
+
+              //"Adding Space"/
+              SizedBox(height: 16),
+
+              //"Our Custom Text Field"/
+              // TxtField(
+              // inputType: TextInputType.text,
+              // hintText: 'Password',
+              // obscureText: true),
+
+              //"Adding Space"/
+              SizedBox(height: 10),
+
+              //"A row with a button at the end"/
+
+              //"Adding Space"/
+              SizedBox(
+                height: 10,
+              ),
+
+              //"custom button"/
+              // Btn(
+              // text: 'Login',
+              // onPressed: () {
+              // Get.to(() => ViewCourses());
+              // },
+
+              //space
+              SizedBox(
+                height: 25,
+              ),
+              InkWell(
+                onTap: () {
+                  Get.to(() => SignUp());
+                },
+                child: RichText(
+                    text: TextSpan(
+                        text: "Don\'t have an account?",
+                        style: TextStyle(color: Colors.black87),
+                        children: <TextSpan>[
+                      TextSpan(
+                          text: " Sign up",
+                          style:
+                              TextStyle(color: Color.fromRGBO(230, 88, 62, 1)))
+                    ])),
+              )
+            ],
           ),
-          color: AppConst.primaryColor,
-        )
-      ]),
+        ),
+      ),
     );
   }
 }
